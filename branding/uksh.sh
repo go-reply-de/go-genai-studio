334b6bdf4905af9c1e4e00509e43f8c3c40f19ef
Omar Chouikha <o.chouikha@goreply.de>
2025-06-17 16:02:32 +0200

    fixed sed

--- branding/uksh.sh
+++ branding/uksh.sh
@@ -10,9 +10,9 @@ NEW_LOGO_PATH="/assets/uksh/Logo_UKSH.png"
 
 ## Check if the files exist before attempting to modify
 if [ -f "$FILE_NAV" ] && [ -f "$FILE_AUTH" ] && [ -f "$FILE_HTML" ]; then
-  sed -i '' "s|${OLD_LOGO_PATH}|${NEW_LOGO_PATH}|g" "$FILE_NAV"
-  sed -i '' "s|${OLD_LOGO_PATH}|${NEW_LOGO_PATH}|g" "$FILE_AUTH"
-  sed -i '' "s|${OLD_LOGO_PATH_HTML}|${NEW_LOGO_PATH}|g" "$FILE_HTML"
+  sed -i "s|${OLD_LOGO_PATH}|${NEW_LOGO_PATH}|g" "$FILE_NAV"
+  sed -i "s|${OLD_LOGO_PATH}|${NEW_LOGO_PATH}|g" "$FILE_AUTH"
+  sed -i "s|${OLD_LOGO_PATH_HTML}|${NEW_LOGO_PATH}|g" "$FILE_HTML"
   echo "Logo branding applied successfully to both files."
 else
   echo "Error: One or both branding target files not found."
