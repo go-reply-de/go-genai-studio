1a2c3377dae8bfa0f05245ed521d40e765dbd86d
Omar Chouikha <o.chouikha@goreply.de>
2025-07-22 15:38:22 +0200

    eslint

--- client/src/routes/TermsOfService.tsx
+++ client/src/routes/TermsOfService.tsx
@@ -5,7 +5,6 @@ import rehypeRaw from 'rehype-raw';
 import { useLocalize } from '~/hooks';
 
 function TermsOfService({ className }: { className?: string }) {
-
   const { data: config } = useGetStartupConfig();
 
   const localize = useLocalize();
@@ -15,31 +14,30 @@ function TermsOfService({ className }: { className?: string }) {
   const modalContent = termsOfService?.modalContent;
   const content = Array.isArray(modalContent)
     ? modalContent.join('\n\n')
-    : modalContent || "The Terms of Service content could not be loaded.";
+    : modalContent || 'The Terms of Service content could not be loaded.';
 
   return (
-    <div className="bg-gray-50 text-gray-800 min-h-screen py-12 font-['Inter',_sans-serif] items-center">
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
-        <header className="text-center mb-10">
-          <h1 className="text-4xl font-bold text-gray-900">{localize("com_ui_terms_of_service")}</h1>
-          <p className="text-lg text-gray-600 mt-2">{localize('com_ui_tos_instructions')}</p>
+    <div className="min-h-screen items-center bg-gray-50 py-12 font-['Inter',_sans-serif] text-gray-800">
+      <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">
+        <header className="mb-10 text-center">
+          <h1 className="text-4xl font-bold text-gray-900">
+            {localize('com_ui_terms_of_service')}
+          </h1>
+          <p className="mt-2 text-lg text-gray-600">{localize('com_ui_tos_instructions')}</p>
         </header>
 
-        <main className="bg-white p-8 rounded-lg shadow-md">
+        <main className="rounded-lg bg-white p-8 shadow-md">
           <div className="prose prose-lg mx-auto">
-            <ReactMarkdown rehypePlugins={[rehypeRaw]}>
-              {content}
-            </ReactMarkdown>
+            <ReactMarkdown rehypePlugins={[rehypeRaw]}>{content}</ReactMarkdown>
           </div>
         </main>
 
-        <footer className="text-center mt-10 text-gray-500 text-sm">
+        <footer className="mt-10 text-center text-sm text-gray-500">
           <p>&copy; {new Date().getFullYear()} Go Reply. All Rights Reserved.</p>
         </footer>
       </div>
     </div>
   );
 }
-
 
 export default TermsOfService;
